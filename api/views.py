51491c3bbab9f55b613ff8208c899d862b3d1352
Fluorky <mrdyrt.inf@gmail.com>
2025-02-22 22:39:37 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import HydroponicSystem, SensorMeasurement
+from .serializers import HydroponicSystemSerializer, SensorMeasurementSerializer
 
-# Create your views here.
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class HydroponicSystemViewSet(viewsets.ModelViewSet):
+    serializer_class = HydroponicSystemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name', 'created_at']
+
+    def get_queryset(self):
+        return HydroponicSystem.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SensorMeasurementViewSet(viewsets.ModelViewSet):
+    serializer_class = SensorMeasurementSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['ph', 'temperature', 'tds']
+    ordering_fields = ['measured_at']
+
+    def get_queryset(self):
+        system_id = self.request.query_params.get('system_id')
+        return SensorMeasurement.objects.filter(system__owner=self.request.user, system__id=system_id)
