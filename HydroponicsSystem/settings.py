5d0ed2912cd80f6004a7fc3da8f8ce881338f771
Fluorky <mrdyrt.inf@gmail.com>
2025-02-24 18:34:53 +0100

    Update setting.py remove unnecessary code

--- HydroponicsSystem/settings.py
+++ HydroponicsSystem/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'drf_yasg',  # Ensure this is included
+    'drf_yasg',
     'api',
 ]
 
