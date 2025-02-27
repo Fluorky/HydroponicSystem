3c3c37be6d0d4642086b145d4050af6a8fceeef5
Fluorky <mrdyrt.inf@gmail.com>
2025-02-27 20:27:13 +0100

    Update serializers.py and views.py to allow feature get_latest_measurements_feature

--- api/serializers.py
+++ api/serializers.py
@@ -4,10 +4,17 @@ from .models import HydroponicSystem, SensorMeasurement
 
 
 class HydroponicSystemSerializer(serializers.ModelSerializer):
+    latest_measurements = serializers.SerializerMethodField()
+
     class Meta:
         model = HydroponicSystem
         fields = '__all__'
         read_only_fields = ['owner']
+
+    def get_latest_measurements(self, obj):
+        """Returns the last 10 measurements for the hydroponic system."""
+        measurements = obj.measurements.order_by('-measured_at')[:10]
+        return SensorMeasurementSerializer(measurements, many=True).data
 
     def validate_name(self, value):
         """Ensure each user cannot create a system with the same name twice."""
